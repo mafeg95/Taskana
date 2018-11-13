1a12bac917ae66ded4a77bf03153346be955593a
Mafe Garcia <mfg21390@gmail.com>
2018-11-13 17:36:43 -0500

    incomplete auth. needs work still

---
+++ frontend/reducers/users_reducer.js
@@ -0,0 +1,17 @@
+import merge from 'lodash/merge';
+
+import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
+
+const usersReducer = (state = {}, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      return merge({}, state, {[action.currentUser.id]: action.currentUser});
+    case RECEIVE_USERS:
+      return action.users;
+    default:
+      return state;
+  }
+};
+
+export default usersReducer;
